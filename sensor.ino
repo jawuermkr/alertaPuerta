cf27d65bf1d4d9922f8748f3121527f159d3b9ad
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2024-06-25 21:13:14 -0500

    Programacion arduino

---
+++ sensor.ino
@@ -0,0 +1,26 @@
+//Puertos del sensor
+const int trigPin = 9;
+const int echoPin = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = (duration * 0.034) / 2;
+  
+  Serial.print("Distancia: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  delay(100);
+}
